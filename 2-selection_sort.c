c820efe6f420fcbb3efdd0ade8d66e4e6ab1a249
GunterPearson <1715@holbertonschool.com>
2020-06-17 17:31:42 +0000

    added comments

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,14 +13,19 @@ void selection_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
+	/* loop thorugh array via size */
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
+		/* set min to first element of array */
+		/* loop through array subtracting one index everytime */
+		/* check current index vs min if current < min, min = current */
 		for (current = i + 1; current < size; current++)
 		{
 			if (array[current] < array[min])
 				min = current;
 		}
+		/* if index i is not the same as min swap with new min */
 		if (i != min)
 		{
 			temp = array[i];
